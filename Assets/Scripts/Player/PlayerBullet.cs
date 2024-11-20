e0ccf08c31c0175949c751416c3a9960618e9e27
minFe17 <vvcx1234@naver.com>
2024-11-20 13:34:31 +0900

    총알 TrailRenderer 버그 수정 (원인 : 오브젝트 풀)

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -4,7 +4,9 @@ using Utils;
 public class PlayerBullet : Bullet
 {
     [SerializeField] EPlayerPoolType _playerPoolType;
+    [SerializeField] TrailRenderer _trailRenderer;
     ObjectPoolManager _objectPoolManager;
+
     void Update()
     {
         Move();
@@ -12,6 +14,7 @@ public class PlayerBullet : Bullet
 
     protected override void Remove()
     {
+        _trailRenderer.Clear();
         if (_objectPoolManager == null)
             _objectPoolManager = GenericSingleton<ObjectPoolManager>.Instance;
         _objectPoolManager.Pull(EPlayerPoolType.Bullet, gameObject);
