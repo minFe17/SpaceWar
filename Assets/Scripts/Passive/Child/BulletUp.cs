cf603bac210cd38f4ff50fe73e9bffa2f5036b0f
minFe17 <119782382+minFe17@users.noreply.github.com>
2025-05-22 17:31:48 +0900

    탄창 용량 증가 패시브 수정
    기존 30 증가에서 2배 증가로

--- Assets/Scripts/Passive/Child/BulletUp.cs
+++ Assets/Scripts/Passive/Child/BulletUp.cs
@@ -2,11 +2,9 @@ using Utils;
 
 public class BulletUp : PassiveData
 {
-    int _bulletUp = 30;
-
     public override void AddPassive()
     {
-        _playerData.MaxBullet += _bulletUp;
+        _playerData.MaxBullet *= 2;
         if (_uiManager == null)
             _uiManager = GenericSingleton<UIManager>.Instance;
         _uiManager.IngameUI.ShowBullet();
